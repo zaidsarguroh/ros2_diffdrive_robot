70c4de1d711f44d94cf53459ad27f0ddeb3059cb
Zaid <zedsargruroh@gmail.com>
2026-08-01 14:34:14 +0200

    fix: update maintainer email

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='zaid',
-    maintainer_email='zaid@todo.todo',
+    maintainer_email='zedsarguroh@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     extras_require={
